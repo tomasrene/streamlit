c44c91022dec7408b2ca16664a9a782b22f85e2b
tomasrene <tomasrene@gmail.com>
2021-07-08 17:29:06 -0300

    v1 de la app de streamlit

--- publisher_streamlit/app.py
+++ publisher_streamlit/app.py
@@ -1,7 +1,17 @@
 
 import streamlit as st
+import pandas as pd
 
 st.title("Heroku")
-st.markdown(
-    "## Como pushear una app de streamlit desde local a heroku"
-)
+st.header("Exploracion")
+st.markdown("Este dataset es un sample de 16.000 filas del dataset original")
+
+data = pd.read_csv("data.csv")
+
+#st.dataframe(data.head())
+st.markdown("## Seleccionar las columnas a visualizar")
+st_ms = st.multiselect("Atributos", data.columns.tolist(), default=["titulo","autor","posicion"])
+st.dataframe(data.loc[:,st_ms])
+st.markdown("## Seleccionar el rango de posiciones a visualizar")
+pos_min,pos_max = st.slider("Posicion",data.posicion.min(),data.posicion.max(),(data.posicion.min(),data.posicion.max()))
+st.dataframe(data[(data.posicion<=pos_max) & (data.posicion>=pos_min)])
