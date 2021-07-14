e184123c57c1b9c96576040ab5f81bf28522ffca
tomasrene <tomasrene@gmail.com>
2021-07-14 13:27:07 -0300

    working version of heroku

--- publisher_streamlit/app.py
+++ publisher_streamlit/app.py
@@ -2,16 +2,17 @@
 import streamlit as st
 import pandas as pd
 
+data = pd.read_csv("data.csv").convert_dtypes()
+
 st.title("Heroku")
 st.header("Exploracion")
 st.markdown("Este dataset es un sample de 16.000 filas del dataset original")
 
-data = pd.read_csv("data.csv")
-
-#st.dataframe(data.head())
 st.markdown("## Seleccionar las columnas a visualizar")
 st_ms = st.multiselect("Atributos", data.columns.tolist(), default=["titulo","autor","posicion"])
 st.dataframe(data.loc[:,st_ms])
 st.markdown("## Seleccionar el rango de posiciones a visualizar")
-pos_min,pos_max = st.slider("Posicion",data.posicion.min(),data.posicion.max(),(data.posicion.min(),data.posicion.max()))
-st.dataframe(data[(data.posicion<=pos_max) & (data.posicion>=pos_min)])
+
+pos_min,pos_max = data.posicion.min().item(),data.posicion.max().item()
+user_min,user_max = st.slider("Posicion",pos_min,pos_max,(pos_min,pos_max))
+st.dataframe(data[(data.posicion<=user_max) & (data.posicion>=user_min)])
